513b2eb88a87a1722d45e0b8b89127296f576b7a
uongductung <42833153+uongductung@users.noreply.github.com>
2018-08-30 16:18:12 +0700

    update 1

--- svnStudy/Controllers/HomeController.cs
+++ svnStudy/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace svnStudy.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+saaaaaaaaaaaaaaaaaaaaaa
             return View();
         }
 
